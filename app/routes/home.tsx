95c14d528ebc9b42bbd22924e2113a398ef8e90a
prth8564 <preetham10107@gmail.com>
2025-08-03 12:50:18 +0530

    resume-card and scoreCirlce component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../components/navbar";
+import { resumes } from "../constants/index";
+import ResumeCard from "../components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -16,5 +18,9 @@ export default function Home() {
         <h2>Review your submissions and check AI powered feedback</h2>
       </div>
     </section>
+    <div className="resumes-section">
+    {resumes.map((resume)=> <ResumeCard key={resume.id} resume={resume} />)}
+
+    </div>
   </main>;
 }
