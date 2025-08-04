78ffcfa81df3410021370f8b9dbe654cf5729370
prth8564 <preetham10107@gmail.com>
2025-08-04 23:53:15 +0530

    auth part2

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router'
 import { usePuterStore } from '~/lib/puter'
 
 export const meta = () =>{
@@ -6,6 +7,16 @@ export const meta = () =>{
 }
 const Auth = () => {
     const {isLoading,auth} = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(auth.isAuthenticated){
+            navigate(next);
+        }
+    },[auth.isAuthenticated,next])
+
   return (
     <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
         <div className="gradient-border shadow-lg">
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "../components/navbar";
 import { resumes } from "../constants/index";
 import ResumeCard from "../components/ResumeCard";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -10,6 +13,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(!auth.isAuthenticated){
+            navigate("/auth?next=/");
+        }
+    },[auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
